ecbf2a1eba0fa3c059eee5723e2bb942239257fe
Oluwasola <sabiduria449@gmail.com>
2025-08-27 03:05:08 +0100

    Create a user Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 
 function App() {
@@ -12,6 +13,20 @@ function App() {
         <WelcomeMessage />
         <Header />
         <MainContent />
+
+        {/* Example UserProgile component usage */}
+        <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+        />
+
+        {/* You can add another UserProfile with different data if you like */}
+        <UserProfile
+        name="Bob"
+        age="30"
+        bio="Enjoys coding and video games"
+        />
         <Footer />
         </div>
       
