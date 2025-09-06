5961d31716fabeb924f431314986374ca76ee18a
Oluwasola <sabiduria449@gmail.com>
2025-09-06 20:14:43 +0100

    A simple Counter Application

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,15 +3,20 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 
 function App() {
 
   return (
-    
+
       <div style={{ fontFamily: 'Arial, sans-serif', margin: 0, padding: 0 }}>
         <Header />
         <UserProfile name="John Doe" age={30} bio="A passionate React developer and avid learner." />
+        <div className="App">
+      <h1>Simple Counter Application</h1>
+      <Counter />
+    </div>
         <MainContent />
         < Footer />
       </div>
