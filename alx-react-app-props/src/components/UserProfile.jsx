25aeff1d07f3b26ebe8658c7b183c6f8861adcf5
Oluwasola <sabiduria449@gmail.com>
2025-09-08 00:08:41 +0100

    Ensure all check pass accurately

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -3,7 +3,17 @@ import UserContext  from "../UserContext";
 import UserInfo from "./UserInfo";
 
 function UserProfile() {
-  return <UserInfo />;
+  const userData = useContext(UserContext);
+
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p><strong>Name:</strong> {userData.name}</p>
+      <p><strong>Email:</strong> {userData.email}</p>
+      <userInfo />
+    </div>
+  );
 }
 
 export default ProfilePage;
