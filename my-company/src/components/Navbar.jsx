ce8e9e073fff979b51e63c5f7a6aeb16984f115f
Oluwasola <sabiduria449@gmail.com>
2025-09-14 12:03:48 +0100

    Create a website from scratch

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    const navStyle = {
+        padding: "10px",
+        background: "#333",
+        color: "#333",
+        display: "flex",
+        justifyContent: "space-around",
+    };
+
+const linkStyle = { color: "#fff", textDecoration: "none" };
+
+return  (
+    <nav style={navStyle}>
+        <Link to="/" style={linkStyle}>Home</Link>
+        <Link to="/about" style={linkStyle}>About</Link>
+        <Link to="/services" style={linkStyle}>Services</Link>
+        <Link to="/contact" style={linkStyle}>Contact</Link>
+    </nav>
+);
+}
+
+export default Navbar;
+
