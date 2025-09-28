b60783789708a615b4a32afb4cf7163474b5cc70
Oluwasola <sabiduria449@gmail.com>
2025-09-28 13:59:19 +0100

    Setting up the GitHub User Search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>GitHub User Search</h1>
+      </header>
+      <main>
+        {/* We will add our Search component here later */}
+        <p>Welcome to the GitHub User Search Application!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
