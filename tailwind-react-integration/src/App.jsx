795dce78f01a903497013feac04ff617fdc7582b
Oluwasola <sabiduria449@gmail.com>
2025-10-08 11:46:16 +0100

    Stlying a Provided React Comp

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,12 @@
-// src/App.jsx
-// ... other imports
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      Hello Tailwind!
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <UserProfile />
     </div>
   );
 }
+
 export default App;
