d4ea4a91a745a978a77ee262639c57b671336e3d
Oluwasola <sabiduria449@gmail.com>
2025-10-10 18:45:03 +0100

    Setting Up a React Application

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 className="text-5xl font-extrabold text-green-700">
+    Recipe Sharing Platform Ready!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
