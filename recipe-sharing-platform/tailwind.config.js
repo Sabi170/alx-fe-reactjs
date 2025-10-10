00b861d7d1f37f3c11dfe2789ae1bc64b7ea9497
Oluwasola <sabiduria449@gmail.com>
2025-10-10 20:05:37 +0100

    Setting Up a React Application for Recipe Sharing

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [], 
+    content: [
+        "./public/index.html",
+        "./src/**/.*/{js,jsx,ts,tsx}",
+    ], 
+    darkMode: false,
     theme: {
         extend: {},
     },
