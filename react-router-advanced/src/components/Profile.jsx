9576199a282f7e3a990b2753a42c394333104fe5
Oluwasola <sabiduria449@gmail.com>
2025-10-23 23:23:26 +0100

    Advanced Routing in React with React Router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,19 +1,28 @@
 // src/components/Profile.jsx
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Routes, Route, useResolvedPath } from 'react-router-dom';
 
 const Profile = () => {
-  return (
+
+    const resolvedPath = useResolvedPath('');
+ return (
     <div style={{ border: '1px solid #ccc', padding: '20px', borderRadius: '8px' }}>
       <h2>User Profile</h2>
       <nav style={{ marginBottom: '15px' }}>
         <ul style={{ listStyle: 'none', padding: 0, display: 'flex', borderBottom: '1px solid #eee' }}>
-          <li style={{ marginRight: '15px', paddingBottom: '5px' }}><Link to="details">Details</Link></li>
-          <li style={{ paddingBottom: '5px' }}><Link to="settings">Settings</Link></li>
+          {/* Use resolvedPath.pathname for relative links */}
+          <li style={{ marginRight: '15px', paddingBottom: '5px' }}><Link to={`${resolvedPath.pathname}/details`}>Details</Link></li>
+          <li style={{ paddingBottom: '5px' }}><Link to={`${resolvedPath.pathname}/settings`}>Settings</Link></li>
         </ul>
       </nav>
-      {/* Outlet renders the matched child route (ProfileDetails or ProfileSettings) */}
-      <Outlet />
+
+      {/* !!! IMPORTANT: Move Routes and Route definitions here !!! */}
+      <Routes>
+        <Route index element={<ProfileDetails />} /> {/* Default child route for /profile */}
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+      {/* No need for <Outlet /> here anymore, as Routes component renders children directly */}
     </div>
   );
 };
